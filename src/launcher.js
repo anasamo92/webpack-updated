9812c556fa32c98c2a5aaba10a9f6e7bd1db2b91
ANA SAMOILA <ASA18330@open-groupe.com>
2021-03-16 15:00:39 +0200

    modif

--- src/launcher.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-import { serverLauncher } from './index';
-// We take the config relative to the process folder, assuming user launch command from project folder
-const webpackConfig = require(process.cwd() + '/webpack.config');
-serverLauncher(webpackConfig, {});
