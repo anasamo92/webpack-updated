9426d2be9f1f6abf7d8dcfc7698ce881d995df9c
ANA SAMOILA <ASA18330@open-groupe.com>
2021-03-16 15:06:41 +0200

    modif

--- launcher.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-var _index = require('./index');
-
-// We take the config relative to the process folder, assuming user launch command from project folder
-var webpackConfig = require(process.cwd() + '/webpack.config');
-(0, _index.serverLauncher)(webpackConfig, {});
